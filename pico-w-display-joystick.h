dd70cf4d86570c8843e270561a0bba5f344b2e5f
Kamil Szyc <kamszyc@gmail.com>
2025-04-05 12:57:24 +0200

    Pico display: refactored joystick up/down logic into header file

---
+++ pico-w-display-joystick.h
@@ -0,0 +1,35 @@
+enum Direction {
+    UP,
+    DOWN
+};
+
+void handle_joystick(Direction direction) {
+    backlight_short->execute();
+
+    if (!backlight->current_values.is_on()) {
+        return;
+    }
+    
+    if (my_display->get_active_page() == page_yeelight) {
+        if (direction == UP)
+            yeelight_control->trigger("yeelight_brighten");
+        else
+            yeelight_control->trigger("yeelight_dim");
+    }
+    else if (my_display->get_active_page() == page_lamp) {
+        if (direction == UP)
+            lamp_control->trigger("lamp_brighten");
+        else
+            lamp_control->trigger("lamp_dim");
+    }
+    else if (my_display->get_active_page() == page_display_brightness) {
+        float b;
+        if (direction == UP)
+            b = std::min(1.0f, backlight->current_values.get_brightness() + 0.1f);
+        else
+            b = std::max(0.1f, backlight->current_values.get_brightness() - 0.1f);
+
+        backlight->make_call().set_brightness(b).perform();
+        my_display->update();
+    }
+}
